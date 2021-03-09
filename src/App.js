aefd79af810467185a61488296efd7a279817cea
ron93 <ronmkare1@gmail.com>
2021-03-09 17:39:42 +0300

    bar chat margin space

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const csvUrl =
   'https://gist.githubusercontent.com/performautodev/ab00b6300b1a235cde9c57600992b86d/raw/9c2f36181b2f090e91dac0b072405b6fe033e60d/UN_Population_2019.csv'
 const width = 960;
 const height = 500;
+const margin = { top: 20 , right: 20, bottom: 20, left:20 };
 
 export const App = () => {
   const [data, setData] = useState(null);
@@ -25,21 +26,30 @@ export const App = () => {
   }
 
   console.log(data[0])
-  
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+
 	const yScale = scaleBand()
   	.domain(data.map(d => d.Country))
-  	.range([0,height]);
+  	.range([0,innerHeight]);
 
   const xScale = scaleLinear()
   	.domain([0, max(data, d => d.Population)])
-  	.range([0,width]);
+  	.range([0,innerWidth]);
   return (
     <svg width={width} height={height}>
-      {data.map(d => <rect x={0} y={yScale(d.Country)} 
-                       width={xScale(d.Population)} 
-                       height={yScale.bandwidth()}>
-                </rect>)}   
-    </svg>
+        <g transform={`translate(${margin.left},${margin.top} )`}>
+
+        {data.map(d =>(
+           <rect x={0} 
+           y={yScale(d.Country)} 
+           width={xScale(d.Population)} 
+           height={yScale.bandwidth()}
+           />
+            ))}   
+             </g>
+             </svg>   
   );
 };
 const rootElement = document.getElementById('root');
