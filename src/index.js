1231e8f2fa0db722cf6586a5e4199ae73d487a15
ron93 <ronmkare1@gmail.com>
2021-03-17 16:10:02 +0300

    main route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App/>
   </BrowserRouter>,
   document.getElementById('root')
@@ -25,4 +25,4 @@ ReactDOM.render(
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
