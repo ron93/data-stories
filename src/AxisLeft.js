b17543874edcd0e2a24cb6969398a32f7f318661
ron93 <ronmkare1@gmail.com>
2021-03-17 14:07:42 +0300

    axis:tick text color

--- src/AxisLeft.js
+++ src/AxisLeft.js
@@ -1,14 +1,15 @@
-
-
-export const AxisLeft = ({yScale}) =>
+export const AxisLeft = ({ yScale }) =>
   yScale.domain().map((tickValue) => (
-    <text 
+    <g className="tick"
+    >
+    <text
       key={tickValue}
       dy="0.32em"
       x={-4}
       y={yScale(tickValue) + yScale.bandwidth() / 2}
-      style={{ textAnchor: "end"  }}
+      style={{ textAnchor: "end" }}
     >
       {tickValue}
     </text>
+    </g>
   ));
