8ae4aa10ada0b80181e03c0006751d2b807455c3
ron93 <ronmkare1@gmail.com>
2021-03-17 12:28:22 +0300

    y axis format tick values

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const csvUrl =
   "https://gist.githubusercontent.com/performautodev/ab00b6300b1a235cde9c57600992b86d/raw/9c2f36181b2f090e91dac0b072405b6fe033e60d/UN_Population_2019.csv";
 const width = 960;
 const height = 500;
-const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const margin = { top: 20, right: 20, bottom: 20, left: 200 };
 
 export const App = () => {
   const [data, setData] = useState(null);
@@ -43,22 +43,30 @@ export const App = () => {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left},${margin.top} )`}>
-       
-       {/* x axis ticks */}
-       {xScale.ticks().map((tickValue) => (
+        {/* x axis ticks */}
+        {xScale.ticks().map((tickValue) => (
           <g transform={`translate(${xScale(tickValue)},0)`}>
             <line y2={innerHeight} stroke="black" />
-            <text dy="0.71em" style={{textAnchor:'middle'}} y={innerHeight+ 4}>{tickValue}</text>
+            <text
+              dy="0.71em"
+              style={{ textAnchor: "middle" }}
+              y={innerHeight + 4}
+            >
+              {tickValue}
+            </text>
           </g>
         ))}
 
         {/* y- axis ticks */}
         {yScale.domain().map((tickValue) => (
-          <g transform={`translate(0,${yScale(tickValue)})`}>
-            <text >
+          <text
+            dy="0.32em"
+            x={-4}
+            y={yScale(tickValue) + yScale.bandwidth() / 2}
+            style={{ textAnchor: "end" }}
+          >
             {tickValue}
-           </text>
-          </g>
+          </text>
         ))}
 
         {data.map((d) => (
