95140b512d190b7566c082fb9638dfcbd6cb1c4f
ron93 <ronmkare1@gmail.com>
2021-03-24 23:10:34 +0300

    browserrouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={"/data-stories"}>
     <App/>
   </BrowserRouter>,
   document.getElementById('root')
