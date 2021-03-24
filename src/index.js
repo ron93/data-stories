0f88322841d0a48d263e956686a0cb45448021d2
ron93 <ronmkare1@gmail.com>
2021-03-24 22:40:41 +0300

    link test - added basename BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
-import { Link } from "react-router-dom";
-
+import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,8 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Link to="/data-stories"></Link>
+  <BrowserRouter basename={"/data-stories"}>
     <App/>
   </BrowserRouter>,
   document.getElementById('root')
